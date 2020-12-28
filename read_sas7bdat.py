bcceb4b15f60ea0eac40be6a5e494d78bb44b9e5
KateRepekh <krepekh@gmail.com>
2020-12-29 01:44:24 +0200

    Change sas7bdat example to avoid pandas

--- read_sas7bdat.py
+++ read_sas7bdat.py
@@ -1,7 +1,12 @@
 from sas7bdat import SAS7BDAT
+from csv import writer
 
 
-def read_example(path_to_read, path_to_save, skip_header=False):
+def read_example(
+    path_to_read, path_to_save, skip_header=False, sample_size=100
+):
     with SAS7BDAT(path_to_read, skip_header=skip_header) as reader:
-        df = reader.to_data_frame()
-        df.iloc[:100].to_csv(path_to_save)
+        with open(path_to_save, "w") as sample_file:
+            csv_writer = writer(sample_file)
+            for row, row_num in zip(reader, range(sample_size)):
+                csv_writer.writerow(row)
